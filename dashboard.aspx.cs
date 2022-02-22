f610581121d315c61826eeb178a7e1a0c0965cb6
Nirali Dadhaniya <niralidadhaniya2001@gmail.com>
2022-02-22 19:18:47 +0530

    modified

--- dashboard.aspx.cs
+++ dashboard.aspx.cs
@@ -5,23 +5,10 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class dashboard : System.Web.UI.Page
+public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        if (Session["email"] == null)
-        {
-            Response.Redirect("~/login.aspx");
-        }
-        else
-        {
-            Literal1.Text = "Welcome ," + Session["email"].ToString();
-        }
-    }
 
-    protected void Button1_Click(object sender, EventArgs e)
-    {
-        Session["email"] = null;
-        Response.Redirect("~/login.aspx");
     }
 }
